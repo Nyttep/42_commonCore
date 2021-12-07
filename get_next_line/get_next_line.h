5b0f9f0de86d22ea3ebef80053c0e90663b4e129
Paul DUBOIS <pdubois@e1r10p21.42.fr>
2021-12-07 14:49:56 +0100

    pushing this

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,7 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pdubois <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/07 14:47:52 by pdubois           #+#    #+#             */
+/*   Updated: 2021/12/07 14:49:19 by pdubois          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H 
-#include <stdlib.h>
-#include <unistd.h>
+
+# include <stdlib.h>
+# include <unistd.h>
+
 char	*get_next_line(int fd);
 int		ft_is_newline(char	*s);
 int		ft_strlen(char	*s);
@@ -9,4 +23,5 @@ char	*ft_kinda_strcat(char	*s, char	*init);
 void	ft_get_reste(char	*s, char	*reste, int read_return);
 char	*ft_cpy_and_rst_reste(char	*ret, char	*reste);
 char	*ft_free(char *ret, char *s);
-# endif
+void	ft_init(char	**s, char	**ret, int	*read_return, char	*reste);
+#endif
