e1984244b702e506f9f3504e5ad64b45a6900467
Nyttep <duboispaul85170@hotmail.fr>
2022-03-31 19:20:43 +0200

    added realloc to libft & continued server

--- minitalk/libft/ft_realloc.c
+++ minitalk/libft/ft_realloc.c
@@ -1,21 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
+/*   ft_realloc.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pdubois <pdubois@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/29 16:59:20 by pdubois           #+#    #+#             */
-/*   Updated: 2022/03/31 19:19:58 by pdubois          ###   ########.fr       */
+/*   Created: 2022/03/31 18:43:32 by pdubois           #+#    #+#             */
+/*   Updated: 2022/03/31 19:19:59 by pdubois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "client.h"
+#include "libft.h"
 
-int	main(void)
+void *ft_realloc(void *ptr, size_t size)
 {
-	char a;
+	char	*ret;
+	int		i;
 
-	a = 0b01100001;
-	ft_printf("%c", a);
+	i = -1;
+	ret = malloc(size);
+	if (!ret)
+		return (-1);
+	ft_bzero(ret, size);
+	while ((char*)ptr + ++i && i < size)
+		ret[i] = (char*) ptr + i;
+	free(ptr);
+	return (ret);
 }
--- minitalk/srcs/server.c
+++ minitalk/srcs/server.c
@@ -6,27 +6,69 @@
 /*   By: pdubois <pdubois@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 17:11:28 by pdubois           #+#    #+#             */
-/*   Updated: 2022/03/30 18:01:00 by pdubois          ###   ########.fr       */
+/*   Updated: 2022/03/31 19:19:57 by pdubois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
 
-void	ft_display_pid()
+void	ft_display_pid(void)
 {
 	ft_printf("%d", getpid());
 }
 
+void	ft_error(char *s)
+{
+	if (s)
+		free(s);
+	exit(-1);
+}
+
+char	*ft_handle_zero(char *ret, int *i)
+{
+	ret[*i / 8] = ret[*i / 8] << 1;
+	*i++;
+	if (*i % 8 == 0 && ret[*i / 9] != 0)
+	{
+		ret = ft_realloc(ret, ft_strlen(ret) + 2);
+		if (!ret)
+			ft_error(ret);
+	}
+}
+
+char	*ft_handle_one(char *ret, int *i)
+{
+
+}
+
 void	ft_handle_signal(int signum)
 {
-	
-	if ()
+	static char	*ret = NULL;
+	static int	i = 0;
+
+	if (!ret)
+		ft_bzero(ret = malloc(2), 2);
+	if (!ret)
+		ft_error(ret);
+	if (signum == SIGUSR1)
+		ret = ft_handle_zero(ret, &i); 
+	if (signum == SIGUSR2)
+		ret = ft_handle_one(ret, &i); 
+	if (ret[i / 9] == 0 && i % 8 == 0)
+	{
+		write(1, ret, ft_strlen(ret));
+		free(ret);
+		ret = NULL;
+		i = 0;
+	}
 }
 
 int	main(void)
 {
 	ft_display_pid();
+	if (signal(SIGUSR1, ft_handle_signal) == SIG_ERR)
+		return (-1);
+	if (signal(SIGUSR2, ft_handle_signal) == SIG_ERR)
+		return (-1);
 	pause();
-	signal(SIGUSR1, ft_handle_signal);
-	signal(SIGUSR2, ft_handle_signal);
 }
