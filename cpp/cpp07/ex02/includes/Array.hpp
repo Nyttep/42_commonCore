2b59af3f44c13850479f26a731f3ec1d0eeb4081
Paul DUBOIS <pdubois@e2r8p16.clusters.42paris.fr>
2022-12-01 16:21:12 +0100

    finished cpp07

--- cpp/cpp07/ex02/includes/Array.hpp
+++ cpp/cpp07/ex02/includes/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: pdubois <pdubois@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 14:11:22 by pdubois           #+#    #+#             */
-/*   Updated: 2022/12/01 15:23:10 by pdubois          ###   ########.fr       */
+/*   Updated: 2022/12/01 16:20:07 by pdubois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,57 @@ class	Array
 {
 	private :
 		T*	_data;
+		size_t	_size;
 	public :
-		Array<T>();
-		Array<T>(const Array<T>& toCopy);
-		Array<T>(size_t n);
-		~Array<T>();
-		Array<T>&	operator=(const Array<T>& toCopy);
-		T&	operator[](size_t i);
-		size_t	size();
+		Array<T>()
+		{
+			std::cout << "Array default constructor called" << std::endl;
+			_data = new T[0]();
+			_size = 0;
+		}
+		Array<T>(const Array<T>& toCopy)
+		{
+			std::cout << "Array copy constructor called" << std::endl;
+			*this = toCopy;
+		}
+		Array<T>(size_t n)
+		{
+			std::cout << "Array args constructor called" << std::endl;
+			_data = new T[n]();
+			_size = n;
+		}
+		~Array<T>()
+		{
+			std::cout << "Array default destructor called" << std::endl;
+			delete [] _data;
+		}
+		Array<T>&	operator=(const Array<T>& toCopy)
+		{
+			this->_size = toCopy.size();
+			delete [] _data;
+			_data = new T[toCopy.size()]();
+			for (int i = 0; 1; i++)
+			{
+				try
+				{
+					_data[i] = toCopy[i];
+				}
+				catch (std::exception& e)
+				{
+					return (*this);
+				}
+			}
+		}
+		T&	operator[](size_t i)
+		{
+			if (i >= size())
+				throw std::exception();
+			return (_data[i]);
+		}
+		size_t	size()
+		{
+			return (_size);
+		}
 };
 
 #endif
--- cpp/cpp07/ex02/srcs/main.cpp
+++ cpp/cpp07/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pdubois <pdubois@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 14:16:34 by pdubois           #+#    #+#             */
-/*   Updated: 2022/12/01 15:20:53 by pdubois          ###   ########.fr       */
+/*   Updated: 2022/12/01 16:20:54 by pdubois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ int	main()
 {
 	Array<int>	arr(10);
 
-	arr[0] = 10;
+	arr[0] = 6;
 	std::cout << "arr[0] = " << arr[0] << std::endl;
 	std::cout << "size of arr = " << arr.size() << std::endl;
-	arr[10];
+	// arr[10]; // will crash because exception not catched
 	return (0);
 }
