a84a04cab0c2cbd1c8b0a5d7bb0543fa75e9ff0f
Nyttep <duboispaul85170@hotmail.fr>
2023-04-01 13:31:01 +0200

    almost finished checking the map

--- cub3d/srcs/check.c
+++ cub3d/srcs/check.c
@@ -6,13 +6,13 @@
 /*   By: pdubois <pdubois@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 18:44:38 by paul              #+#    #+#             */
-/*   Updated: 2023/02/07 14:44:15 by pdubois          ###   ########.fr       */
+/*   Updated: 2023/03/31 11:36:29 by pdubois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-void	ft_check(int ac, char **av)
+void	ft_check_basic(int ac, char **av)
 {
 	int	len_av;
 
@@ -85,4 +85,36 @@ int	ft_check_rgb(char *str)
 		i++;
 	}
 	return (0);
+}
+
+int	ft_check_map(t_game *game)
+{
+	int		i;
+	int		j;
+	char	c;
+	
+	i = 0;
+	while (game->map[i])
+	{
+		j = 0;
+		while (game->map[i][j])
+		{
+			c = game->map[i][j];
+			if (!ft_is_valid_char_map(c))
+				ft_error(game, "The map is not valid");
+			if (c == '0' || c == 'N' || c == 'S' || c == 'E' || c == 'W')
+			{
+				if (game->map[i][j - 1] == ' ')
+					ft_error(game, "The map is not valid");
+				if (game->map[i][j + 1] == ' ')
+					ft_error(game, "The map is not valid");
+				if (game->map[i - 1][j] == ' ')
+					ft_error(game, "The map is not valid");
+				if (game->map[i + 1][j] == ' ')
+					ft_error(game, "The map is not valid");
+			}
+			j++;
+		}
+		i++;
+	}
 }
--- cub3d/srcs/initialization_utils.c
+++ cub3d/srcs/initialization_utils.c
@@ -6,7 +6,7 @@
 /*   By: pdubois <pdubois@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 18:54:26 by pdubois           #+#    #+#             */
-/*   Updated: 2023/02/08 16:15:42 by pdubois          ###   ########.fr       */
+/*   Updated: 2023/03/31 11:04:15 by pdubois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,4 +43,42 @@ char	*ft_format_path(char *buff)
 	buff = ft_skip_spaces(buff + i);
 	buff[ft_strlen(buff) - 1] = 0;
 	return ((buff));
+}
+
+int	ft_is_valid_char_map(char c)
+{
+	if (c == ' ')
+		return (1);
+	if (c == '1')
+		return (1);
+	if (c == '0')
+		return (1);
+	if (c == 'N')
+		return (1);
+	if (c == 'S')
+		return (1);
+	if (c == 'E')
+		return (1);
+	if (c == 'W')
+		return (1);
+	return (0);
+}
+
+int	ft_max_length(char **strs)
+{
+	int	ret;
+	int	tmp;
+	int	i;
+
+	i = 0;
+	tmp = 0;
+	ret = 0;
+	while (strs[i])
+	{
+		tmp = ft_strlen(strs[i]);
+		if (tmp > ret)
+			ret = tmp;
+		i++;
+	}
+	return (ret)
 }
