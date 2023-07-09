16e9a5aa913b0b98f6e76144291fb447d8a7bf9a
Nyttep <duboispaul85170@hotmail.fr>
2023-07-09 17:51:46 +0200

    reminding me how to c++, it's getting better

---
+++ cpp/cpp09/ex00/includes/BitcoinExchange.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pdubois <pdubois@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/09 17:06:32 by pdubois           #+#    #+#             */
+/*   Updated: 2023/07/09 17:42:45 by pdubois          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+# include <iostream>
+# include <fstream>
+# include <string>
+# include <map>
+
+class	BitcoinExchange
+{
+	private :
+		std::map<std::string, float>	_rate;
+		std::map<std::string, float>	_amount;
+	public :
+		BitcoinExchange();
+		BitcoinExchange(std::map<std::string, float> newDB);
+		BitcoinExchange(const BitcoinExchange& toCopy);
+		~BitcoinExchange();
+		BitcoinExchange& operator=(const BitcoinExchange& toCopy);
+		void	init(int fdRate, int fdAmount);
+
+}
+
+#endif 
